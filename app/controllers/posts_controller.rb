6739480d44b9d923b71067de734bbce20c9535eb
Nader Ben chaaben <naderbenchaaben@episousse.com.tn>
2021-02-18 14:44:30 +0100

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,45 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "user", password:"user", except: [:index, :show]
     def index
+        @posts = Post.all
+        
+    end
+    def show
+        @post = Post.find(params[:id])
     end
     def new
+        @post = Post.new
     end
     def create
         #render plain: params[:post].inspect
-        @post = Post.new(params[:post])
+        @post = Post.new(post_params)
+        if(@post.save)
+        redirect_to @post
+        else
+          render 'new'
+        end
+      end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post
+            else
+              render 'edit'
+            end
+            
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post = Post.delete(params[:id])
+        redirect_to posts_path
+    
+    end  
     
     end
-end
+    private def post_params
+        params.require(:post).permit(:title, :body)
+    end
+
